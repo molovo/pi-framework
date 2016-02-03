03e1d38519723ddfd71d43669dd6fe4fdc35bea2
James Dinsdale <hi@molovo.co.uk>
2016-02-03 23:03:15 +0000

    Add routes command to cli help

--- src/Pug/Cli/templates/help/help.php
+++ src/Pug/Cli/templates/help/help.php
@@ -6,4 +6,5 @@ return <<<EOF
 
 \e[0;33mCommands:\e[0;m
     \e[0;32mcompile\e[0;m     Compile static resources for the site.
+    \e[0;32mroutes\e[0;m      Display a table containing all the app's routes.
 EOF;
