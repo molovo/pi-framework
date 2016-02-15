946693b918d0be1bb43a8a6d5e515845bd80642d
James Dinsdale <hi@molovo.co.uk>
2016-02-15 20:25:35 +0000

    Tidying and better documentation of Session

--- src/Pug/Framework/Session.php
+++ src/Pug/Framework/Session.php
@@ -6,15 +6,33 @@ use Pug\Framework\Session\Handler;
 
 class Session
 {
+    /**
+     * The session handler.
+     *
+     * @var Handler
+     */
     public static $handler = null;
 
+    /**
+     * Bootstrap the session.
+     *
+     * @param Config $config The session config
+     */
     public static function bootstrap(Config $config)
     {
         static::$handler = new Handler($config);
         session_set_save_handler(static::$handler, true);
         session_start();
+        session_register_shutdown();
     }
 
+    /**
+     * Get a value from the session.
+     *
+     * @param string $key The session key
+     *
+     * @return mixed The value
+     */
     public static function get($key)
     {
         if (isset($_SESSION[$key])) {
@@ -24,8 +42,14 @@ class Session
         return;
     }
 
+    /**
+     * Store a value in the session.
+     *
+     * @param string $key   The session key
+     * @param mixed  $value The value to set
+     */
     public static function set($key, $value)
     {
-        return $_SESSION[$key] = $value;
+        $_SESSION[$key] = $value;
     }
 }
--- src/Pug/Framework/Session/Handler.php
+++ src/Pug/Framework/Session/Handler.php
@@ -9,24 +9,55 @@ use Pug\Framework\Config;
 
 class Handler implements \SessionHandlerInterface
 {
+    /**
+     * The cache instance in which session data will be stored.
+     *
+     * @var CacheInstance
+     */
     private $storage = null;
 
+    /**
+     * Create the session handler.
+     *
+     * @param Config $config The session config
+     */
     public function __construct(Config $config)
     {
-        $config        = new CacheConfig($config->toArray());
+        // Convert the config object into one the cache understands
+        $config = new CacheConfig($config->toArray());
+
+        // Create the cache instance for use in reading/writing session data
         $this->storage = new CacheInstance('pug_session_storage', $config);
     }
 
+    /**
+     * @param string $savePath    The session save path (Not used).
+     * @param string $sessionName The session name (Not used).
+     *
+     * @return bool Check that the cache instance has been created successfully
+     */
     public function open($savePath, $sessionName)
     {
-        return true;
+        return ($this->storage instanceof CacheInstance);
     }
 
+    /**
+     * Close the session (Not used).
+     *
+     * @return bool
+     */
     public function close()
     {
         return true;
     }
 
+    /**
+     * Retrieve the session payload from the cache, and decrypt it.
+     *
+     * @param string $id The ID of the current session
+     *
+     * @return string The serialized session data
+     */
     public function read($id)
     {
         if (($payload = $this->storage->get('session.'.$id, false)) !== null) {
@@ -36,6 +67,14 @@ class Handler implements \SessionHandlerInterface
         return;
     }
 
+    /**
+     * Encrypt the session data, and store it in the cache.
+     *
+     * @param string $id   The ID of the current session
+     * @param string $data The serialized session data
+     *
+     * @return bool
+     */
     public function write($id, $data)
     {
         if (!empty($data)) {
@@ -45,6 +84,13 @@ class Handler implements \SessionHandlerInterface
         return true;
     }
 
+    /**
+     * Clear the session data from the cache.
+     *
+     * @param string $id The ID of the current session
+     *
+     * @return bool
+     */
     public function destroy($id)
     {
         $this->storage->clear('session.'.$id);
@@ -52,6 +98,13 @@ class Handler implements \SessionHandlerInterface
         return true;
     }
 
+    /**
+     * Garbage collection (Not used).
+     *
+     * @param int $maxlifetime
+     *
+     * @return bool
+     */
     public function gc($maxlifetime)
     {
         return true;
