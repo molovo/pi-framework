8ea73b1978b345c92653e7f5a0824465dc72fbed
James Dinsdale <hi@molovo.co.uk>
2016-02-15 23:33:59 +0000

    Add cookie class

---
+++ src/Pug/Http/Cookie.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Pug\Http;
+
+use Pug\Framework\Application;
+
+class Cookie
+{
+    public static function get($key)
+    {
+        if (isset($_COOKIE[$key])) {
+            return $_COOKIE[$key];
+        }
+
+        return;
+    }
+
+    public static function set($key, $value, $expiry = 2592000)
+    {
+        $app     = Application::instance();
+        $path    = $app->config->app->base_uri ?: '/';
+        $domain  = $app->config->app->domain ?: $_SERVER['HTTP_HOST'];
+        $secure  = ($_SERVER['REQUEST_SCHEME'] === 'https');
+        $expires = time() + $expiry;
+        setcookie($key, $value, $expires, $path, $domain, $secure, true);
+    }
+}
